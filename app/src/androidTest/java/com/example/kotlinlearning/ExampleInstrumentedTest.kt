4441c348268de44abc026a95917f69d19ec82f35
zhavei <zhavei.vei@gmail.com>
2022-02-01 22:28:10 +0700

    ad unit test

--- app/src/androidTest/java/com/example/kotlinlearning/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/kotlinlearning/ExampleInstrumentedTest.kt
@@ -1,12 +1,20 @@
 package com.example.kotlinlearning
 
-import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.typeText
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
-
+import androidx.test.platform.app.InstrumentationRegistry
+import org.hamcrest.Matchers.containsString
+import org.junit.Assert.assertEquals
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-
-import org.junit.Assert.*
+import java.util.regex.Pattern.matches as matches1
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -20,5 +28,22 @@ class ExampleInstrumentedTest {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("com.example.kotlinlearning", appContext.packageName)
+    }
+}
+@RunWith(AndroidJUnit4::class)
+class CalculatorTest{
+    @get:Rule()
+    val activity = ActivityScenarioRule(CalculatorTips::class.java)
+
+    @Test
+    fun calculate_20_percent_tip(){
+        onView(withId(R.id.cost_of_service_editText))
+            .perform(typeText("50.00"))
+
+        onView(withId(R.id.mButtonTwo)).perform(click())
+
+        onView(withId(R.id.tip_result))
+            .check(matches(withText(containsString("10.00"))))
+
     }
 }
--- app/src/main/java/com/example/kotlinlearning/CalculatorTips.kt
+++ app/src/main/java/com/example/kotlinlearning/CalculatorTips.kt
@@ -1,6 +1,10 @@
 package com.example.kotlinlearning
 
+import android.content.Context
 import android.os.Bundle
+import android.view.KeyEvent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import androidx.appcompat.app.AppCompatActivity
 import com.example.kotlinlearning.databinding.ActivityCalculatorTipsBinding
 import com.google.android.material.snackbar.Snackbar
@@ -15,15 +19,17 @@ class CalculatorTips : AppCompatActivity() {
         binding = ActivityCalculatorTipsBinding.inflate(layoutInflater)
         setContentView(binding.root)
         binding.mButtonTwo.setOnClickListener { calculateTip() }
-
+        binding.costOfServiceEditText.setOnKeyListener { view, keyCode, _ ->
+            handlekeyEvent(view, keyCode) }
     }
 
     private fun calculateTip() {
-        val stringTextField = binding.costOfService.text.toString()
+        val stringTextField = binding.costOfServiceEditText.text.toString()
 
         val cost = stringTextField.toDoubleOrNull()
         if (cost == null || cost == 0.0) {
-            Snackbar.make(binding.root, "Please Fill the Cost Service", Snackbar.LENGTH_SHORT).show()
+            Snackbar.make(binding.root, "Please Fill the Cost Service", Snackbar.LENGTH_SHORT)
+                .show()
             displayTip(0.0)
             return
         }
@@ -45,6 +51,16 @@ class CalculatorTips : AppCompatActivity() {
     private fun displayTip(tipMoney: Double) {
         val formatedTip = NumberFormat.getCurrencyInstance().format(tipMoney)
         binding.tipResult.text = getString(R.string.tip_amount, formatedTip)
+    }
+
+    private fun handlekeyEvent(view: View, keyCode: Int): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_ENTER) {
+            val inputMethodManager =
+                getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+            return true
+        }
+        return false
     }
 
 }
